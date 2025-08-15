e3c232caafb286e0fc72f8d9d2d0453888d4027b
DanielHernandez <daniel@asesoriasysoluciones.com>
2025-08-15 12:33:31 -0500

    Initial commit: Fabric 1.21.8 template setup

---
+++ src/client/java/com/pedalhat/lategameplus/LateGamePlusClient.java
@@ -0,0 +1,10 @@
+package com.pedalhat.lategameplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LateGamePlusClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/pedalhat/lategameplus/LateGamePlusDataGenerator.java
@@ -0,0 +1,11 @@
+package com.pedalhat.lategameplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LateGamePlusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
