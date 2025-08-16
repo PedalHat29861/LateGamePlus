b1c367f63a697c2d8838b443e02c688818226cf6
PedalHat29861 <78623410+PedalHat29861@users.noreply.github.com>
2025-08-16 12:25:11 -0500

    Add netherite nugget drop to piglin brutes

--- src/main/java/com/pedalhat/lategameplus/LateGamePlus.java
+++ src/main/java/com/pedalhat/lategameplus/LateGamePlus.java
@@ -3,8 +3,15 @@ package com.pedalhat.lategameplus;
 import com.pedalhat.lategameplus.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +27,16 @@ public class LateGamePlus implements ModInitializer {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.addAfter(Items.NETHERITE_INGOT, ModItems.NETHERITE_NUGGET);
+        });
+
+        LootTableEvents.MODIFY.register((id, tableBuilder, source) -> {
+            if (id.equals(EntityType.PIGLIN_BRUTE.getLootTableKey().orElseThrow())) {
+                LootPool.Builder pool = LootPool.builder()
+                        .conditionally(RandomChanceLootCondition.builder(0.125f))
+                        .with(ItemEntry.builder(ModItems.NETHERITE_NUGGET))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 2.0f)));
+                tableBuilder.pool(pool);
+            }
         });
     }
 }
