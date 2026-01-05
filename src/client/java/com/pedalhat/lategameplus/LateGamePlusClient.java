f57cb5090e0757eb311d2778101323e6e0d269c3
PedalHat29861 <78623410+PedalHat29861@users.noreply.github.com>
2026-01-05 01:34:41 -0500

    feat: agregar texturas y modelo para el bloque Fusion Forge, incluyendo animaciones de fuego

--- src/client/java/com/pedalhat/lategameplus/LateGamePlusClient.java
+++ src/client/java/com/pedalhat/lategameplus/LateGamePlusClient.java
@@ -1,18 +1,22 @@
 package com.pedalhat.lategameplus;
 
+import com.pedalhat.lategameplus.registry.ModBlocks;
 import com.pedalhat.lategameplus.registry.ModScreenHandlers;
 import com.pedalhat.lategameplus.screen.FusionForgeScreen;
 import com.pedalhat.lategameplus.util.AnimationSoundSynchronizer;
 import com.pedalhat.lategameplus.util.TimeBridge;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.render.BlockRenderLayer;
 
 public class LateGamePlusClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         HandledScreens.register(ModScreenHandlers.FUSION_FORGE, FusionForgeScreen::new);
+        BlockRenderLayerMap.putBlock(ModBlocks.FUSION_FORGE, BlockRenderLayer.CUTOUT);
 
         TimeBridge.setNowSupplier(() -> {
             MinecraftClient mc = MinecraftClient.getInstance();
